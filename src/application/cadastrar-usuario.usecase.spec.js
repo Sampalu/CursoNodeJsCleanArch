90051961524cd034d960f37698e06a0a0f6eeb3f
sampalu <andreluisampaio@gmail.com>
2024-11-30 19:07:44 -0300

    criando testes de erro

--- src/application/cadastrar-usuario.usecase.spec.js
+++ src/application/cadastrar-usuario.usecase.spec.js
@@ -1,3 +1,4 @@
+const AppError = require("../shared/errors/AppError");
 const cadastrarUsuarioUseCase = require("./cadastrar-usuario.usecase");
 
 describe('Cadastrar usuario UseCase', function () {
@@ -20,5 +21,9 @@ describe('Cadastrar usuario UseCase', function () {
     expect(output).toBeUndefined();
     expect(usuariosRepository.cadastrar).toHaveBeenCalledWith(usuarioDTO);
     expect(usuariosRepository.cadastrar).toHaveBeenCalledTimes(1);
+  });
+
+  test('Deve retornar um throw AppError se o usuariosRepository não for fornecido ', async function () {
+    expect(() => cadastrarUsuarioUseCase({})).toThrow(new AppError(AppError.dependencias));
   });
 });
